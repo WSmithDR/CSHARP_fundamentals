7f37dae9e6c8505833623a7f6062396a19e0396f
WSmithDR <wsduenas@espol.edu.ec>
2026-02-03 14:19:40 -0500

    Implementa GetPersonByIdUseCase y elimina clase vacía
    Se añade la clase GetPersonByIdUseCase para obtener una persona por su ID usando un repositorio genérico y se elimina la clase vacía Class1. Además, se actualizan los metadatos de ensamblado y el hash de caché para reflejar los cambios.

---
+++ Domain/Application/UseCases/Persons/GetPersonByIdUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.Persons
+{
+    public class GetPersonByIdUseCase
+    {
+        private IRepository<PersonEntity, Guid> _repository;
+
+        public GetPersonByIdUseCase(IRepository<PersonEntity, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<PersonEntity> ExecuteAsync(Guid id)
+        {
+            var person = await _repository.GetByIdAsync(id);
+
+            if(person == null)
+            {
+                throw new InvalidOperationException($"No se encontró una persona con el id: {id}");
+            }
+
+            return person;
+        }
+    }
+}
--- Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003b23dd15af5cb0fd89b488231ca61e9fae69a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89035b1206b32a3d0d6524a450587089932ed8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
