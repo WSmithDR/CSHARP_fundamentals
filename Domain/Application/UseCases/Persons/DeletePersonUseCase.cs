b7b8e96d32d0bd4b786cd039b3780c394ee12846
WSmithDR <wsduenas@espol.edu.ec>
2026-02-03 18:49:19 -0500

    Añadir caso de uso para eliminar personas
    Se implementa la clase DeletePersonUseCase que permite eliminar una entidad PersonEntity mediante un repositorio genérico. Se actualiza la versión del ensamblado y los archivos relacionados.

---
+++ Domain/Application/UseCases/Persons/DeletePersonUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.Persons
+{
+    public class DeletePersonUseCase
+    {
+        private readonly IRepository<PersonEntity, Guid> _repository;
+
+        public DeletePersonUseCase(IRepository<PersonEntity, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Execute(Guid id)
+        {
+            var foundPerson = await _repository.GetByIdAsync(id);
+            if (foundPerson == null)
+            {
+                throw new InvalidOperationException($"No se encontró persona con el id: {id}");
+            }
+            else
+            {
+                await _repository.DeleteAsync(foundPerson);
+                await _repository.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f37dae9e6c8505833623a7f6062396a19e0396f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dc64a6bcd05b60c98510fcaabad04fd5f76d65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
