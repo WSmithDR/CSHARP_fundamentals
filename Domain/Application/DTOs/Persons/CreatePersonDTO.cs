25c21a0043094a70563372e75b810751ef3f4205
WSmithDR <wsduenas@espol.edu.ec>
2026-02-03 22:57:42 -0500

    Añade UpdatePersonDTO y propiedad Code en CreatePersonDTO
    - Se añade la propiedad Code a CreatePersonDTO.
    - Se crea la clase UpdatePersonDTO con propiedades básicas.
    - Se actualizan los metadatos de versión en archivos de ensamblado.

--- Domain/Application/DTOs/Persons/CreatePersonDTO.cs
+++ Domain/Application/DTOs/Persons/CreatePersonDTO.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Application.DTOs.Persons
 {
     public class CreatePersonDTO
     {
+        public string Code { get; private set; } = string.Empty;
         public string FirstName { get; private set; } = string.Empty;
         public string LasttName { get; private set; } = string.Empty;
         public string Email { get; private set; } = string.Empty;
---
+++ Domain/Application/DTOs/Persons/UpdatePersonDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Persons
+{
+    public class UpdatePersonDTO
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; } = string.Empty;
+        public string LastName { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public string Cellphone { get; private set; } = string.Empty;
+    }
+}
