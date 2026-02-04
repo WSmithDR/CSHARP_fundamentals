7c970c02824316cffac9933b32649e743b7af72f
WSmithDR <wsduenas@espol.edu.ec>
2026-02-03 22:50:50 -0500

    Refactor: mueve CreatePersonDTO a nuevo namespace
    Se ha trasladado la clase CreatePersonDTO al namespace Application.DTOs.Persons y restaurado las declaraciones using. También se ha actualizado la versión de ensamblado y el hash de caché para reflejar los cambios. No se han modificado las propiedades de la clase.

--- Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b8e96d32d0bd4b786cd039b3780c394ee12846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b56942870e60f8a5a4179454de78e57723a4b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
