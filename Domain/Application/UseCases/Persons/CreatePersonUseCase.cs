b347cdc0e57e3f68cd7e07e158161e6ecdd3c4b7
WSmithDR <wsduenas@espol.edu.ec>
2026-02-03 23:40:52 -0500

    Añadir CreatePersonUseCase y actualizar metadatos de ensamblado
    Se implementa la clase CreatePersonUseCase para gestionar la creación de nuevas personas, validando la unicidad del código antes de guardar. También se actualizan los metadatos de ensamblado reflejando el nuevo hash de versión.

---
+++ Domain/Application/UseCases/Persons/CreatePersonUseCase.cs
@@ -0,0 +1,47 @@
+﻿using Application.DTOs.Persons;
+using Domain;
+using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.Persons
+{
+    public class CreatePersonUseCase
+    {
+        private readonly IRepository<PersonEntity, Guid> _repository;
+
+        private readonly ICodeRepository<PersonEntity> _codeRepository;
+
+        public CreatePersonUseCase(
+            IRepository<PersonEntity, Guid> repository, 
+            ICodeRepository<PersonEntity> codeRepository
+            )
+        {
+            _repository = repository;
+            _codeRepository = codeRepository;
+        }
+
+        public async Task<PersonEntity> ExecuteAsync(CreatePersonDTO dto)
+        {
+            if(await _codeRepository.ExistsWithCodeAsync(dto.Code))
+            {
+                throw new InvalidOperationException($"Ya existe una person con el codigo: {dto.Code}");
+            }
+            else
+            {
+                var newPerson = new PersonEntity(
+                        dto.Code,
+                        dto.FirstName,
+                        dto.LasttName,
+                        dto.Email,
+                        dto.PhoneNumber
+                        );
+
+                await _repository.AddAsync(newPerson);   
+                await _repository.SaveChangesAsync();
+                return newPerson;
+            }
+        }
+    }
+}
