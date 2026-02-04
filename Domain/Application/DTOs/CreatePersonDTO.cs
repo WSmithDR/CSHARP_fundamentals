08b56942870e60f8a5a4179454de78e57723a4b5
WSmithDR <wsduenas@espol.edu.ec>
2026-02-03 22:47:48 -0500

    Añadir DTO CreatePerson y actualizar metadatos de ensamblado
    Se agrega el archivo CreatePersonDTO.cs con la clase CreatePersonDTO en Application.DTOs. Se actualizan los metadatos de versión en AssemblyInfo y el hash en AssemblyInfoInputs.cache para reflejar los nuevos cambios. Nota: posible error tipográfico en "LasttName".

---
+++ Domain/Application/DTOs/CreatePersonDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class CreatePersonDTO
+    {
+        public string FirstName { get; private set; } = string.Empty;
+        public string LasttName { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public string PhoneNumber { get; private set; } = string.Empty;
+    }
+}
--- Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Domain/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dc64a6bcd05b60c98510fcaabad04fd5f76d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b8e96d32d0bd4b786cd039b3780c394ee12846")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
